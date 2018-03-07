00d30c323715c6c588c955caf72013fdf7102a20
Johnny Madrid <johnny.madrid-heredia@capgemini.com>
2018-03-07 09:24:50 +0000

    Formatted Files and Developed MuleComponent

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -1,22 +1,29 @@
 package com.mulesoft.training;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MuleComponent {
 
-  public Map<String,String> processMap() {
-    // processMap implementation
-    return null;
-  }
+	public Map<String, String> processMap(Map<String, String> input) {
+		input.put("processedBy", "processMap");
+		return input;
+	}
 
-  public Map<String,String> processArray() {
-    // processArray implementation
-    return null;
-  }
+	public Map<String, String> processArray(List<String> input) {
+		Map<String, String> output = new HashMap<String, String>();
+		output.put("message", input.get(0));
+		output.put("processedBy", "processedArray");
 
-  public Map<String,String> processString() {
-    // processString implementation
-    return null;
-  }
+		return output;
+	}
 
-} 
+	public Map<String, String> processString(String input) {
+		Map<String, String> output = new HashMap<String, String>();
+		output.put("message", input);
+		output.put("processedBy", "processedArray");
+		return null;
+	}
+
+}
